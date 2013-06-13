d444c7d70ced9b8d03b35d20ec557ff8586df210
krh5058 <ken.r.hwang@gmail.com>
2013-06-13 18:05:16 -0400

    061313 #5
    Added collision detection

--- src/RaceGame.java
+++ src/RaceGame.java
@@ -1,12 +1,17 @@
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -20,19 +25,21 @@ import javax.swing.KeyStroke;
 public class RaceGame extends JFrame implements Runnable, ActionListener{
 	static RaceGame frame; // Static frame for easy reference
 	protected CustomPanel newPanel;
-	static int fWidth = 1200;
-	static int fHeight = 900;
+	static int fWidth = 1000;
+	static int fHeight = 750;
 	protected double curX = fWidth/2;
 	protected double curY = fHeight/2;
 	private Container cp;
 	private BufferedImage p1img;
+	public Rectangle p1rect;
 	private BufferedImage p1crash;
-	private BufferedImage p2img;
+	public BufferedImage p2img;
+	public Rectangle p2rect;
 	private BufferedImage p2crash;
 	public boolean[] keys = new boolean[7];
 	private double carSpeed = 0;
 	private double carDir = 0;
-	private double accelIncrement = .05;
+	private double accelIncrement = .025;
 	private double turnIncrement = Math.PI/90;
 	private double turnMax = 360;
 	//create menu items
@@ -83,14 +90,17 @@ public class RaceGame extends JFrame implements Runnable, ActionListener{
         
         //Load images
         try {
-			p1img = ImageIO.read(getClass().getResource("Red.png"));
-			p1crash = ImageIO.read(getClass().getResource("RedCrash.png"));
-			p2img = ImageIO.read(getClass().getResource("Blue.png"));
-			p2crash = ImageIO.read(getClass().getResource("BlueCrash.png"));
+			p1img = ImageIO.read(getClass().getResource("Red_50x30.png"));
+			p1crash = ImageIO.read(getClass().getResource("RedCrash_50x30.png"));
+			p2img = ImageIO.read(getClass().getResource("Blue_50x30.png"));
+			p2crash = ImageIO.read(getClass().getResource("BlueCrash_50x30.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+        
+		p1rect = new Rectangle(0,0,p1img.getWidth(),p1img.getHeight());
+		p2rect = new Rectangle(0,0,p1img.getWidth(),p1img.getHeight());
         
 	}
 	
@@ -109,7 +119,9 @@ public class RaceGame extends JFrame implements Runnable, ActionListener{
 	
 	public class CustomPanel extends JPanel  {
 
-		private static final long serialVersionUID = 1L;
+		public Shape p1trans;
+		
+		private static final long serialVersionUID = -1516251819657951269L;
 		public CustomPanel(){
 			System.out.println("CustomPanel");
 		}
@@ -120,8 +132,46 @@ public class RaceGame extends JFrame implements Runnable, ActionListener{
 			at.rotate(carDir);
 			at.translate(-p1img.getWidth()/2,-p1img.getHeight()/2);
 			Graphics2D g2d = (Graphics2D) g;
-			g2d.drawImage(p1img, at, null);
+//			g2d.drawImage(p1img, at, null);
+			
+			g.setColor(Color.red);
+			g.fillRect((int)p2rect.x,(int)p2rect.y,(int)p2rect.getWidth(),(int)p2rect.getHeight());
+			
+//			p1rect = new Rectangle((int)Math.ceil(getcurX()),(int)Math.ceil(getcurY()),p1img.getWidth(),p1img.getHeight());
+//			g2d.setPaint(Color.red);
+//			g2d.fillRect((int)p1rect.x,(int)p1rect.y,(int)p1rect.getWidth(),(int)p1rect.getHeight());
+//			AffineTransform at2 = AffineTransform.getTranslateInstance(p1rect.x,p1rect.y);
+//			if (keys[4]){
+			p1trans = at.createTransformedShape(p1rect);
+	    	checkCollision();
+//			if (p1trans.intersects(p2rect)) {
+//				System.out.println("Collision!");
+//			}
+			g2d.draw(at.createTransformedShape(p1rect));
+//			}
+//			System.out.println(p1rect.getBounds());
 		}
+		
+		public Shape getTrans(){
+			return p1trans;
+		}
+		
+	    public void checkCollision() {
+	    	
+//	    	if (getTrans().intersects(p2rect)) {
+//	    		System.out.println("Collision!");
+//	    	}
+//	    	if (p1rect.intersects(p2rect)){
+//	    		System.out.println("Collision");
+//	    	}
+//	    	System.out.println(p1trans.getBounds());
+//	    	System.out.println(p2rect.getBounds());
+	    	Area areaA = new Area(p2rect);
+	    	areaA.intersect(new Area(p1trans));
+	    	if(!areaA.isEmpty()) {
+	    		System.out.println("Collision!");
+	    	}
+	    }
 	}
 
 	
@@ -140,6 +190,9 @@ public class RaceGame extends JFrame implements Runnable, ActionListener{
 				}
 				if (theEvent.getKeyCode()==KeyEvent.VK_RIGHT){
 					keys[3] = true;
+				}
+				if (theEvent.getKeyCode()==KeyEvent.VK_NUMPAD0){
+					keys[4] = true;
 				}
 		}
 		
@@ -203,6 +256,7 @@ public class RaceGame extends JFrame implements Runnable, ActionListener{
     	}
     	
     }
+   
     
     public void accel(double v) {
     	carSpeed+=v;
@@ -232,8 +286,8 @@ public class RaceGame extends JFrame implements Runnable, ActionListener{
     		xCalc = 0;
     		carSpeed = 0;
     	}
-    	if (yCalc > (fHeight - p1img.getWidth())) {
-    		yCalc = (fHeight - p1img.getWidth());
+    	if (yCalc > (fHeight - 2*p1img.getWidth())) {
+    		yCalc = (fHeight - 2*p1img.getWidth());
     		carSpeed = 0;
     	}
     	if (yCalc < 0) {
